56ff7370935bae9f169dd57b2bc7ede02748706b
Marco Agatensi <marcoagatensi@gmail.com>
2026-07-29 19:02:25 +0200

    Refactoring of CarRepositoryJpaTest extracting a BaseRepositoryTest class to be extended by all repository test classes

---
+++ com.marcoaga02.car-rental-manager/src/test/java/com/marcoaga02/carrentalmanager/repository/BaseRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.marcoaga02.carrentalmanager.repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+@Testcontainers
+public abstract class BaseRepositoryTest {
+
+	@SuppressWarnings("resource")
+	@Container
+	private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:18.4")
+			.withDatabaseName("carrental_test")
+			.withUsername("test")
+			.withPassword("test");
+
+	private EntityManagerFactory entityManagerFactory;
+
+	protected EntityManager entityManager;
+
+	@BeforeEach
+	void baseSetUp() {
+		Map<String, String> properties = new HashMap<>();
+		properties.put("jakarta.persistence.jdbc.url", postgres.getJdbcUrl());
+		properties.put("jakarta.persistence.jdbc.user", postgres.getUsername());
+		properties.put("jakarta.persistence.jdbc.password", postgres.getPassword());
+
+		entityManagerFactory = Persistence.createEntityManagerFactory("test-pu", properties);
+		entityManager = entityManagerFactory.createEntityManager();
+	}
+
+	@AfterEach
+	void baseTearDown() {
+		if (entityManager != null) {
+			entityManager.close();
+		}
+		if (entityManagerFactory != null) {
+			entityManagerFactory.close();
+		}
+	}
+
+}
