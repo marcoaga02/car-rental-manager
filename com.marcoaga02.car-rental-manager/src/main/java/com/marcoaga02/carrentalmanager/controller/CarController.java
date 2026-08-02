93ce29a69fc074bc74af29e1bc9512d7f9d6f983
Marco Agatensi <marcoagatensi@gmail.com>
2026-08-02 12:14:02 +0200

    Updated BaseView and CarController with the clearFields method

--- com.marcoaga02.car-rental-manager/src/main/java/com/marcoaga02/carrentalmanager/controller/CarController.java
+++ com.marcoaga02.car-rental-manager/src/main/java/com/marcoaga02/carrentalmanager/controller/CarController.java
@@ -25,6 +25,7 @@ public class CarController {
 	public void createCar(CarViewModel request) {
 		try {
 			carService.createCar(request);
+			carView.clearFields();
 			this.getAllCars();
 		} catch (IllegalArgumentException | DuplicateCarPlateException e) {
 			carView.showError(e.getMessage());
